4516871d80aa9fff58ad43bfbba4b981a94b2fad
sbondil <sbondil@gmail.com>
2020-04-21 21:37:54 -0500

    2 files modified, 2 deleted - Entrega vf

--- index.js
+++ index.js
@@ -27,37 +27,7 @@ function make_viz(data){
       .draw()
 }
 
-var data2 = [
-    {"year": 1991, "name":"alpha", "value": 15},
-    {"year": 1991, "name":"beta", "value": 10},
-    {"year": 1991, "name":"gamma", "value": 5},
-    {"year": 1991, "name":"delta", "value": 50},
-    {"year": 1992, "name":"alpha", "value": 20},
-    {"year": 1992, "name":"beta", "value": 10},
-    {"year": 1992, "name":"gamma", "value": 10},
-    {"year": 1992, "name":"delta", "value": 43},
-    {"year": 1993, "name":"alpha", "value": 30},
-    {"year": 1993, "name":"beta", "value": 40},
-    {"year": 1993, "name":"gamma", "value": 20},
-    {"year": 1993, "name":"delta", "value": 17},
-    {"year": 1994, "name":"alpha", "value": 60},
-    {"year": 1994, "name":"beta", "value": 60},
-    {"year": 1994, "name":"gamma", "value": 25},
-    {"year": 1994, "name":"delta", "value": 32}
-  ]
 
-  function make_viz2(data2){
-  var visualization = d3plus.viz()
-      .container("#viz2")
-      .data(data2)
-      .type("tree_map")
-      .id("name")
-      .size("value")
-      .time("year")
-      .draw()
-}
-
-make_viz2(data2)
 
 // Sankey Graph
 var edges = [
